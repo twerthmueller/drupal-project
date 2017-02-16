3ad3359d876b656cdc66cc3805e605517f8a3a6f
Thomas Werthmüller <twerthmueller@previon.ch>
2017-02-16 16:44:26 +0100

    behat.yaml removed

--- web/modules/custom/one_two/tests/src/Functional/BrowserTest.php
+++ web/modules/custom/one_two/tests/src/Functional/BrowserTest.php
@@ -10,11 +10,17 @@ use Drupal\Tests\BrowserTestBase;
  */
 class BrowserTest extends BrowserTestBase {
 
+
+
   /**
    * Test register.
    */
   function testDrupalGet() {
     $this->drupalGet('user/register');
-    $this->assertSession()->pageTextContains('Create new account');
+    $this->assertSession()->pageTextContains( 'Create new account');
+    $this->assertSession()->fieldExists( 'Email address');
+    $this->assertSession()->fieldExists( 'Username');
+    $this->assertSession()->buttonExists( 'Create new account');
+    $this->assertSession()->pageTextNotContains( 'Joomla');
   }
 }
